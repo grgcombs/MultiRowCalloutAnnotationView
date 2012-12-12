18917578bbaa853ab15adb652c0f8b09fd1947bd
Greg Combs <gregory.combs@oracle.com>
2012-12-12 09:26:11 -0600

    Updated the license headers for some of the files, to match the rest of the project.

--- MultiRowCalloutAnnotationView/MultiRowAnnotationProtocol.h
+++ MultiRowCalloutAnnotationView/MultiRowAnnotationProtocol.h
@@ -2,10 +2,10 @@
 //  MultiRowAnnotationProtocol.h
 //  Created by Greg Combs on 11/30/11.
 //
-//  OpenStates by Sunlight Foundation, based on work at https://github.com/sunlightlabs/StatesLege
+//  based on work at https://github.com/grgcombs/MultiRowCalloutAnnotationView
 //
-//  This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License. 
-//  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/
+//  This work is licensed under the Creative Commons Attribution 3.0 Unported License. 
+//  To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/
 //  or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 //
 //
